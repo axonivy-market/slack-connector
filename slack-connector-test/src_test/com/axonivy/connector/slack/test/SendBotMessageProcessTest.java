057e4ae7bd642f94397cb89d59684f52aad40da9
quanpham-axonivy <quan.phamvan@axonactive.com>
2026-05-19 16:15:16 +0700

    create test

---
+++ slack-connector-test/src_test/com/axonivy/connector/slack/test/SendBotMessageProcessTest.java
@@ -0,0 +1,28 @@
+package com.axonivy.connector.slack.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+
+@IvyProcessTest
+public class SendBotMessageProcessTest {
+	private static final BpmProcess SEND_BOT_MESSAGE_PROCESS = BpmProcess.path("SendBotMessage");
+	private static final BpmElement SEND_BOT_MESSAGE_CALLABLE = SEND_BOT_MESSAGE_PROCESS
+			.elementName("sendBotMessage(String,String,String)");
+
+	@Test
+	public void testSendBotMessage(BpmClient bpmClient) {
+		String channel = "test channel";
+		String botToken = "adsad123qwd";
+		String message = "Task is created";
+		ExecutionResult result = bpmClient.start().subProcess(SEND_BOT_MESSAGE_CALLABLE).execute(botToken, channel,
+				message);
+		assertTrue(result.bpmError() == null);
+	}
+}
