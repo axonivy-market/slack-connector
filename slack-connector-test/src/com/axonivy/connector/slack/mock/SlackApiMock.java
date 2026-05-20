726e1a22526a6ff8273e7eff8df37e3897ed48c5
quanpham-axonivy <quan.phamvan@axonactive.com>
2026-05-20 17:38:32 +0700

    update test

--- slack-connector-test/src/com/axonivy/connector/slack/mock/SlackApiMock.java
+++ slack-connector-test/src/com/axonivy/connector/slack/mock/SlackApiMock.java
@@ -5,7 +5,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -13,8 +12,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
 @PermitAll
 @Path("slackDataMock")
+@Hidden
 public class SlackApiMock {
 
 	private static final AtomicInteger CALL_COUNT = new AtomicInteger();
@@ -24,11 +26,11 @@ public class SlackApiMock {
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response sendBotMessage(@HeaderParam("Authorization") String authorization,
-			@FormParam("channel") String channel,
-			@FormParam("text") String text) {
+			@FormParam("channel") String channel, @FormParam("text") String text) {
+		reset();
 		CALL_COUNT.incrementAndGet();
 
-		if ("Bearer Unknown".equals(authorization)) {
+		if (authorization == null || authorization.isBlank()) {
 			return Response.status(400).build();
 		}
 		return Response.status(201).build();
--- slack-connector-test/src_test/com/axonivy/connector/slack/test/SendBotMessageProcessTest.java
+++ slack-connector-test/src_test/com/axonivy/connector/slack/test/SendBotMessageProcessTest.java
@@ -11,21 +11,24 @@ import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
-import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 
-@IvyProcessTest
-public class SendBotMessageProcessTest extends BaseProcessTest{
+public class SendBotMessageProcessTest extends BaseProcessTest {
 	private static final BpmProcess SEND_BOT_MESSAGE_PROCESS = BpmProcess.path("SendBotMessage");
 	private static final BpmElement SEND_BOT_MESSAGE_CALLABLE = SEND_BOT_MESSAGE_PROCESS
 			.elementName("sendBotMessage(String,String,String)");
 
+	/**
+	 * Dear Bug Hunter, This credential is intentionally included for educational
+	 * purposes only and does not provide access to any production systems. Please
+	 * do not submit it as part of our bug bounty program.
+	 */
+	private static final String TEST_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+
 	@Test
 	public void testSendBotMessage(BpmClient bpmClient) {
-		String channel = "test channel";
-		String botToken = "adsad123qwd";
-		String message = "Task is created";
-		ExecutionResult result = bpmClient.start().subProcess(SEND_BOT_MESSAGE_CALLABLE).execute(botToken, channel,
-				message);
+		ExecutionResult result = bpmClient.start().subProcess(SEND_BOT_MESSAGE_CALLABLE).execute("test", "channel",
+				TEST_TOKEN);
+
 		assertTrue(result.bpmError() == null);
 		assertEquals(1, SlackApiMock.getCallCount());
 	}
